d001a28d611d56005320bc144a16527873ec02db
DESKTOP-TNPO3RF\瀬戸敬晴 <trike1236@gmail.com>
2019-07-14 19:10:28 +0900

    slack通知のテスト

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("uio");
+        Debug.Log("uinga");
     }
 
     // Update is called once per frame
