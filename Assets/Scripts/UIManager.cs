e9aa4a5fcd67e3c3a7ddc3ffbc0711cc314ac3f4
DESKTOP-TNPO3RF\瀬戸敬晴 <trike1236@gmail.com>
2019-07-15 04:09:54 +0900

    配列操作わからん　Debug機能をもうちょっとなんとかしたい

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public char[,] puzzleWordsForDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzleWordsForDisplay = new char[6,6];
+        puzzleWordsForDisplay[1, 2] = 't';
+        puzzleWordsForDisplay[1, 3] = 'a';
+        puzzleWordsForDisplay[1, 4] = 'k';
+        puzzleWordsForDisplay[1, 5] = 'o';
+        Debug.Log(puzzleWordsForDisplay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
