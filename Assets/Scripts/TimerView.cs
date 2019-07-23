5d1d6450295bd9e3f5bec2b9391828bf49707636
DESKTOP-TNPO3RF\瀬戸敬晴 <trike1236@gmail.com>
2019-07-24 00:41:19 +0900

    add: UI manager

---
+++ Assets/Scripts/TimerView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+
+public class TimerView : MonoBehaviour
+{
+    [SerializeField]
+    private UIManager UIManager;
+    [SerializeField]
+    private Text counterText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIManager.OnTimeChanged.Subscribe(puzzleWords =>
+        {
+            string text = "";
+            puzzleWords.ForEach(s => text += (s + "\n"));
+            counterText.text = text;
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using System;
 
 public class UIManager : MonoBehaviour
 {
 
     public char[,] puzzleWordsForDisplay;
+    public List<string> puzzleWords = new List<string>();
+
+    private Subject<List<string>> timeSubject = new Subject<List<string>>();
+
+    public IObservable<List<string>> OnTimeChanged
+    {
+        get { return timeSubject; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(TimeCoroutine());
         puzzleWordsForDisplay = new char[6,6];
         puzzleWordsForDisplay[1, 2] = 't';
         puzzleWordsForDisplay[1, 3] = 'a';
@@ -17,6 +29,17 @@ public class UIManager : MonoBehaviour
         Debug.Log(puzzleWordsForDisplay);
     }
 
+    IEnumerator TimeCoroutine()
+    {
+        var time = 30;
+        while (time > 0)
+        {
+            puzzleWords.Add(time.ToString());
+            timeSubject.OnNext(puzzleWords);
+            yield return new WaitForSeconds(1);
+            time--;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
